5fc5d13716655e92ff2358306b45532899814e24
MManojManu <manojmanu15319@gmail.com>
2017-02-14 18:45:12 +0530

    updated

--- news_agrgreator_analyzer_ui/search/views.py
+++ news_agrgreator_analyzer_ui/search/views.py
@@ -1,27 +1,8 @@
-from __future__ import unicode_literals
 
 from django.views import generic
 import MySQLdb
 from collections import deque
-from django.shortcuts import render_to_response
-from sphinxit.core.processor import Search
-from django.http import JsonResponse
-from django.http import HttpResponse
-
-from django.views.generic import ListView
-
-
-"""class SphinxitConfig(object):
-    DEBUG = True
-    WITH_META = True
-    WITH_STATUS = False
-    POOL_SIZE = 5
-    SQL_ENGINE = 'mysql'
-    SEARCHD_CONNECTION = {
-        'host': '127.0.0.1',
-        'port': 9306,
-    }
-"""
+from django.shortcuts import render
 
 
 class Connector(object):
@@ -52,10 +33,10 @@ class SphinxRecord(object):
     def __init__(self):
         self.sphinx_connector = Connector(host="127.0.0.1", port=9306, options={})
 
-    def get_sphinx_record(self, query):
+    def get_sphinx_record(self, query, args=None):
         sphinx_con = self.sphinx_connector.get_connection()
         sphinx_cursor = sphinx_con.cursor()
-        sphinx_cursor.execute(query)
+        sphinx_cursor.execute(query, args or ())
         sphinx_details = sphinx_cursor.fetchall()
         self.sphinx_connector.put_connection(sphinx_con)
         return sphinx_details
@@ -73,9 +54,9 @@ class CreateQuery(object):
         if not search_keyword:
             from_match = "where match('')"
         else:
-            from_match = "where match('%s')" % search_keyword
+            from_match = "where id=%s" % search_keyword
 
-        limit = "limit 10"
+        limit = ""
         final_query = self.query.format(fields=fields, match=from_match, limit=limit)
 
         return final_query
@@ -83,26 +64,31 @@ class CreateQuery(object):
 
 class IndexView(generic.ListView):
     paginate_by = 5
-    template_name = 'search/result.html'
+    template_name = 'search/form.html'
     context_object_name = 'sphinx_details'
 
-    """def get_queryset(request):
-        search_query = Search(indexes=['newsdb', ], config=SphinxitConfig)
-        search_query = search_query.match('karnataka')
-        search_result = search_query.ask()
-
-        return JsonResponse(search_result)"""
-
-    def get_queryset(request):
-        obj_query = CreateQuery()
+    def get_queryset(self, **kwargs):
         obj_search = SphinxRecord()
-
-        search_keyword = "tamilnadu"
-
-        final_query = obj_query.get_final_query(search_keyword)
+        final_query = "select * from newsdb order by published_date desc limit 5"
         sphinx_details = obj_search.get_sphinx_record(final_query)
+        return sphinx_details
 
-        if not sphinx_details:
-            print("No Records Found!!")
-        else:
-            return (sphinx_details)
+
+def index_view(request, search):
+    template_name = 'search/view.html'
+    obj_query = CreateQuery()
+    obj_search = SphinxRecord()
+    final_query = obj_query.get_final_query(search)
+    sphinx_details = obj_search.get_sphinx_record(final_query)
+
+    return render(request, template_name, {'sphinx_details': sphinx_details})
+
+
+def detailed_view(request, pk):
+    template_name = 'search/view.html'
+    obj_query = CreateQuery()
+    obj_search = SphinxRecord()
+    final_query = obj_query.get_final_query(pk)
+    sphinx_details = obj_search.get_sphinx_record(final_query)
+
+    return render(request, template_name, {'sphinx_details': sphinx_details})
